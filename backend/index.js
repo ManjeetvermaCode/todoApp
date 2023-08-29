33d003189b96db2207e87928e813e5901d93cf4a
manjeetverma <manjeetverma@gmail.com>
2023-08-29 22:23:35 +0530

    interagrated frontend and backend and also resolved cors issue

--- backend/index.js
+++ backend/index.js
@@ -1,16 +1,39 @@
 import express from 'express'
+const app=express()
 import dotenv from 'dotenv'
 import dbConfig from './connectDb.js'
+// import cors from 'cors'
 
-const app=express()
+import tasks from './models/taskModel.js'
+import users from './models/userModel.js'
+
 
 dotenv.config()
-
 dbConfig()
+// app.use(cors())
+// // app.use(express.static(path.join(__dirname, '/frontend/dist')));
 
-app.get('/',(req,res)=>{
+// app.get('*', (req, res) =>
+//   res.sendFile(path.resolve(__dirname, 'frontend', 'dist', 'index.html'))
+// );
+
+app.get('/api/',(req,res)=>{
     res.send('home page')
 })
+
+app.get('/api/tasks',async(req,res)=>{
+    const allTasks=await tasks.find({})
+    res.status(200).json(allTasks)
+
+})
+
+app.get('/api/users',async(req,res)=>{
+    const allUsers=await users.find({}).populate('tasks')
+    res.status(200).json(allUsers)
+
+})
+
+
 
 
 app.listen(3000,()=>{
---
+++ frontend/vite-project/src/screens/HomeScreen.jsx
@@ -0,0 +1,22 @@
+import axios from 'axios'
+export default function HomeScreen() {
+    try {
+        axios.get('http://localhost:3000/api/tasks')
+        .then((response) => {
+          const allTasks = response.data; // Access the response data here
+          console.log(allTasks);
+        })
+        .catch((error) => {
+          console.error('Error:', error);
+        });
+    } catch (error) {
+        console.log(error)
+    }
+
+    return(
+    <>
+        <h1>This is HomeScreen</h1>
+    </>
+    )
+    
+}
